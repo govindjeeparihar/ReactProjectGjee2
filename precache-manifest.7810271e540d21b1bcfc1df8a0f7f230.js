3ce34aa6e2e83d53bf6325aa0b1d3b2818dc5cf6
GovindJeeParihar <govindam88@gmail.com>
2020-09-22 11:07:38 +0530

    Updates

---
+++ precache-manifest.7810271e540d21b1bcfc1df8a0f7f230.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9205923d87b7e09ca1b9318e8c2f0f23",
+    "url": "/ReactProjectGjee2/index.html"
+  },
+  {
+    "revision": "828f24a8e38c4553de45",
+    "url": "/ReactProjectGjee2/static/css/main.c97707ac.chunk.css"
+  },
+  {
+    "revision": "b1fd7cbbfd90ed72f088",
+    "url": "/ReactProjectGjee2/static/js/2.24b0a01c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/ReactProjectGjee2/static/js/2.24b0a01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828f24a8e38c4553de45",
+    "url": "/ReactProjectGjee2/static/js/main.a5262aa2.chunk.js"
+  },
+  {
+    "revision": "7e2d00c51955e6347677",
+    "url": "/ReactProjectGjee2/static/js/runtime-main.3e5b2573.js"
+  },
+  {
+    "revision": "499eaf2440fd7f9054cc32db249beeb5",
+    "url": "/ReactProjectGjee2/static/media/about.499eaf24.jpg"
+  },
+  {
+    "revision": "15fa2fe81ad65199abeb19420e990e61",
+    "url": "/ReactProjectGjee2/static/media/boxicons.15fa2fe8.ttf"
+  },
+  {
+    "revision": "2722ed7ea574bdf612fa903ac3c20730",
+    "url": "/ReactProjectGjee2/static/media/boxicons.2722ed7e.eot"
+  },
+  {
+    "revision": "42d2adbbb48ee4c1c5fc6e4c01e1ab04",
+    "url": "/ReactProjectGjee2/static/media/boxicons.42d2adbb.woff2"
+  },
+  {
+    "revision": "b8fb7c66c4bd6e25db8b9d1d7f0fa2d2",
+    "url": "/ReactProjectGjee2/static/media/boxicons.b8fb7c66.woff"
+  },
+  {
+    "revision": "c1f1800dcec2b24f452fe8086bc5ed5e",
+    "url": "/ReactProjectGjee2/static/media/boxicons.c1f1800d.svg"
+  },
+  {
+    "revision": "c5daca9a0e337f6181db269b74081c7d",
+    "url": "/ReactProjectGjee2/static/media/hero-bg.c5daca9a.jpg"
+  },
+  {
+    "revision": "2eb96321ab48d0ba8c87de31ad7087fe",
+    "url": "/ReactProjectGjee2/static/media/testimonials-1.2eb96321.jpg"
+  },
+  {
+    "revision": "b6d1701868336600a333ea7bec662425",
+    "url": "/ReactProjectGjee2/static/media/testimonials-2.b6d17018.jpg"
+  },
+  {
+    "revision": "3d677828b23ca5dc9653eb9e8f92aa1f",
+    "url": "/ReactProjectGjee2/static/media/testimonials-3.3d677828.jpg"
+  },
+  {
+    "revision": "08d8679b1e421ad63fd4d61156a5acd1",
+    "url": "/ReactProjectGjee2/static/media/testimonials-4.08d8679b.jpg"
+  },
+  {
+    "revision": "72fb04435870ed091240bd9da7474dab",
+    "url": "/ReactProjectGjee2/static/media/testimonials-5.72fb0443.jpg"
+  },
+  {
+    "revision": "b13e3dc2831f733c246cbf4169da9bde",
+    "url": "/ReactProjectGjee2/static/media/why-us.b13e3dc2.jpg"
+  }
+]);
